e6ee945cb1845395642db078a0068a8e0debd155
Hanna Israielian <hann092u@stud.kea.dk>
2023-11-23 12:33:24 +0100

    mobil

--- src/scripts/menu.js
+++ src/scripts/menu.js
@@ -2,6 +2,6 @@ const burgerMenu = document.querySelector("#burger-menu");
 const menu = document.querySelector("#menu");
 
 burgerMenu.addEventListener("click", function () {
-  this.classList.toggle("close");
+  burgerMenu.classList.toggle("close");
   menu.classList.toggle("overlay");
 });
